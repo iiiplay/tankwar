e08310aed7bdd91abf2b14d665c220a249f19888
iiiplay001 <iiiplay001@gmail.com>
2020-07-01 11:11:58 +0800

    顯示主介面跟我方坦克

--- src/main/java/com/app17/tankwar/GameClient.java
+++ src/main/java/com/app17/tankwar/GameClient.java
@@ -18,14 +18,15 @@ public class GameClient extends JComponent {
     }
 
     public static void main(String[] args) {
-
         JFrame frame = new JFrame();
         frame.setTitle("來了!第一個坦克大戰!!");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
         GameClient client=new GameClient();
         frame.repaint();
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
 
