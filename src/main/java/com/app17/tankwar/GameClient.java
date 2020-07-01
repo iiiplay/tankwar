6dbc884e53888fc0c754481c0b0f89ec5d9774f5
iiiplay001 <iiiplay001@gmail.com>
2020-07-01 14:47:36 +0800

    加入敵方坦克

--- src/main/java/com/app17/tankwar/GameClient.java
+++ src/main/java/com/app17/tankwar/GameClient.java
@@ -4,20 +4,35 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameClient extends JComponent {
 
     private Tank playerTank;
 
+    private List<Tank> enemyTanks;
+
     private GameClient() {
         this.setPreferredSize(new Dimension(800, 600));
         this.playerTank = new Tank(400, 100, Direction.DOWN);
+
+        enemyTanks = new ArrayList<>(12);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                enemyTanks.add(new Tank(200 + j * 120, 400 + 40 * i, Direction.UP, true));
+            }
+        }
     }
 
 
     @Override
     protected void paintComponent(Graphics g) {
         playerTank.draw(g);
+        for (Tank tank : enemyTanks) {
+            tank.draw(g);
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/app17/tankwar/Tank.java
+++ src/main/java/com/app17/tankwar/Tank.java
@@ -10,6 +10,20 @@ public class Tank {
     private int y;
     private Direction direction;
     private boolean stopped;
+    private boolean enemy;
+
+
+    public Tank(int x, int y, Direction direction) {
+        this(x, y, direction, false);
+    }
+
+    public Tank(int x, int y, Direction direction, boolean enemy) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.enemy = enemy;
+    }
+
 
     public int getX() {
         return x;
@@ -27,11 +41,6 @@ public class Tank {
         this.y = y;
     }
 
-    public Tank(int x, int y, Direction direction) {
-        this.x = x;
-        this.y = y;
-        this.direction = direction;
-    }
 
     void move() {
         if (stopped)
@@ -70,23 +79,24 @@ public class Tank {
     }
 
     Image getImage() {
+        String prefix = enemy ? "e" : "";
         switch (direction) {
             case UP:
-                return new ImageIcon("assets/images/tankU.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankU.gif").getImage();
             case DOWN:
-                return new ImageIcon("assets/images/tankD.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankD.gif").getImage();
             case LEFT:
-                return new ImageIcon("assets/images/tankL.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankL.gif").getImage();
             case RIGHT:
-                return new ImageIcon("assets/images/tankR.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankR.gif").getImage();
             case UP_RIGHT:
-                return new ImageIcon("assets/images/tankRU.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankRU.gif").getImage();
             case UP_LEFT:
-                return new ImageIcon("assets/images/tankLU.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankLU.gif").getImage();
             case DOWN_LEFT:
-                return new ImageIcon("assets/images/tankLD.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankLD.gif").getImage();
             case DOWN_RIGHT:
-                return new ImageIcon("assets/images/tankRD.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankRD.gif").getImage();
         }
 
         return null;
